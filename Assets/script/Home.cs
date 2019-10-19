5ea132ba5b938dd0f8ead3111ead37a9fef0dddc
Jirapong Rungsawang <newsonesnsd@gmail.com>
2019-10-19 11:14:26 +0700

    [ADD] Username to Home
    Add username to Home Scence
    EDIT Scrool Set To Top

---
+++ Assets/script/Home.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Home : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text emailText;
+    void Start()
+    {
+        emailText.text = User.usermail;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
